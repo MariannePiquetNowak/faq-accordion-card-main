5ef220a7fc2bbc6226deb35cfbe1161fcf59132d
Marianne <mariannexpiquet@gmail.com>
2021-01-21 15:19:54 +0100

    rotate the arrow when accordion is active

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ let section = document.getElementsByClassName('accordeon');
 
 for(let i = 0; i < section.length; i++) {
     section[i].addEventListener("click", () => {
-        // ajoute et retire la class active
+        // ajoute et retire la class active 
         section[i].classList.toggle("active");
 
+        // Cible la flèche
+        let arrow = section[i].children[0].children[1]
+
+        // Je cible l'enfant souhaité (la div ".accordeon-item-parr")
         let panel = section[i].children[1];
 
+        // Si section contient une class active
         if (section[i].classList.contains("active")) {
+            // Je fais apparaitre son enfant "a"ccordeon-item-parr"
             panel.style.display = "block";
+            arrow.style.transform = "rotate(180deg)";
         } else {
+            // Si, je le "cache"
             panel.style.display = "none";
-        }
-
-
-
-        // Faire apparaitre l'enfant voulu => section[i].children
-        
+            arrow.style.transform = "rotate(360deg)";
+        }  
     })
 
 }
+
